83d19afa946e56a5a784328f5c0f29f429a8a46f
Célian Garcia <celian.garcia1@gmail.com>
2018-11-11 17:17:52 +0100

    Effectively call the ability cinéma

--- cmd/oratio/main.go
+++ cmd/oratio/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
+	"milobella/oratio/pkg/ability"
 	"milobella/oratio/pkg/anima"
 	"milobella/oratio/pkg/cerebro"
 	"net/http"
@@ -14,6 +15,7 @@ import (
 
 var cerebroCli = cerebro.NewClient("http://0.0.0.0", 9444)
 var animaCli = anima.NewClient("http://0.0.0.0", 9333)
+var cinemaCli = ability.NewClient("http://0.0.0.0", 10200)
 
 // fun main()
 func main() {
@@ -25,6 +27,7 @@ func main() {
 type RequestBody struct {
     Text 	string   `json:"text,omitempty"`
 }
+
 type ResponseBody struct {
     Vocal  	string `json:"vocal,omitempty"`
 }
@@ -39,7 +42,7 @@ func TextRequest(w http.ResponseWriter, r *http.Request) {
 
 	// Execute the processing flow
 	var nlu = cerebroCli.UnderstandText(body.Text)
-	var nlg = CallSphere(nlu)
+	var nlg = CallAbility(nlu)
 	var vox = animaCli.GenerateSentence(nlg)
 
 	// Build the response
@@ -56,14 +59,21 @@ func ReadRequest(r *http.Request) (req RequestBody, err error) {
 	return
 }
 
-// TODO: call the sphere according to the intent
-func CallSphere(nlu cerebro.NLU) anima.NLG {
+func CallAbility(nlu cerebro.NLU) anima.NLG {
+	// TODO put personal request in anima
 	if nlu.Intent == "HELLO"{
 		return anima.NLG{Sentence: "Hello"}
 	}
+
+	// TODO put time request in clock ability
 	if nlu.Intent == "GET_TIME" {
 		timeVal := fmt.Sprintf("%d h %d", time.Now().Hour(), time.Now().Minute())
 		return anima.NLG{Sentence: "It is {{time}}", Params: map[string]string{"time": timeVal}}
 	}
+
+	if nlu.Intent == "LAST_SHOWTIME" {
+		return cinemaCli.CallAbility(nlu)
+	}
+
 	return anima.NLG{Sentence: "Oups !"}
 }
---
+++ pkg/ability/client.go
@@ -0,0 +1,72 @@
+package ability
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"milobella/abilities/ability-sdk-go/pkg/ability"
+	"milobella/oratio/pkg/anima"
+	"milobella/oratio/pkg/cerebro"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Client struct {
+	host string
+	port int
+	url string
+	client http.Client
+}
+
+func NewClient(host string, port int) *Client {
+	url := strings.Join([]string{host, strconv.Itoa(port)}, ":")
+	return &Client{host: host, port: port, url: url, client: http.Client{}}
+}
+
+func (c Client) makeRequest(request ability.Request) (response ability.Response, err error) {
+	restituteEndpoint := strings.Join([]string{c.url, "resolve", request.Nlu.Intent}, "/")
+	jsonNLG, err := json.Marshal(request)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	req, err := http.NewRequest("POST", restituteEndpoint, bytes.NewBuffer(jsonNLG))
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	return
+}
+
+func (c Client) CallAbility(nlu cerebro.NLU) (nlg anima.NLG) {
+	request := ability.Request{Nlu: nlu}
+	result, err := c.makeRequest(request)
+	if err != nil {
+		log.Print(err)
+		nlg.Sentence = "error"
+		return
+	}
+
+	nlg = result.Nlg
+	return
+}
